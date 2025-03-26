47674bfe1d2f955617129034eb613c9423d8bb83
Harshal Gahlot <gahlotharshal@gmail.com>
2025-03-26 10:19:19 +0530

    basic complex struct

---
+++ C/chapter 9/complex_numer.c
@@ -0,0 +1,17 @@
+// Write a program with a structure representing a complex number
+
+#include <stdio.h>
+
+struct complex_number
+{
+    int a;
+    int b;
+};
+
+
+int main () {
+    struct complex_number c1 = {2, 3};
+    
+
+    return 0;
+}
