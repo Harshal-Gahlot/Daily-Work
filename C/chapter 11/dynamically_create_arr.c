cea0567d3d2ab126737e5678dc7bb8127d9cbd25
Harshal Gahlot <gahlotharshal@gmail.com>
2025-03-30 22:33:43 +0530

    dynamically create an array i.e. malloc()

---
+++ C/chapter 11/dynamically_create_arr.c
@@ -0,0 +1,23 @@
+// Write a program to dynamically create an array of size 6 capable of storing 6 integers.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int length = 6;
+    // int arr[length];
+    int* ptr;
+    ptr = (int*)malloc(length * sizeof(int));
+
+    ptr[0]=1;
+    ptr[1]=2;
+    ptr[2]=3;
+    ptr[3]=4;
+    ptr[4]=5;
+    ptr[5]=6;
+
+    printf("%d",ptr[5]);
+    
+
+    return 0;
+}
---
+++ C/chapter 11/get_user_int_in_dynamically_created_arr.c
@@ -0,0 +1,2 @@
+// Use the array in problem 1 to store 6 integers entered by the user.
+
