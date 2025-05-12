aef251a3aecd0bec9d3f505781e3fb79cdcdc69b
Harshal <gahlotharshal@gmail.com>
2025-05-12 18:26:54 +0530

    continued learning tailwind

--- web dev harkirat/week-13-tailwind/src/App.jsx
+++ web dev harkirat/week-13-tailwind/src/App.jsx
@@ -3,9 +3,14 @@ import './App.css';
 function App() {
 
 	return (
-		<>
-			<div className='bg-blue-300'>hi</div>
-		</>
+		<main className='bg-blue-950 h-screen w-screen flex flex-col items-center'>
+			<h1 className='mt-28'>webinar.gg</h1>
+			<h2>Verify your age</h2>
+			<p>pls conferm ur dob this data will not be stored</p>
+			<input className='bg-blue-900 outline-2-red' type="date" name="" value='Your Birth Date' id="" />
+			<input className='bg-gray-600' type="submit" value="submit" />
+			
+		</main>
 	);
 }
 
