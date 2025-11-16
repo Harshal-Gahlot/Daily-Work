233cb65a9d6d7b6e037e8a87e7152ad170139cc1
Harshal <gahlotharshal@gmail.com>
2025-11-16 23:56:26 +0530

    1/2 of week13 2nd part completed. also today i watched a mlvid taught at MIT by lex, was pr overwhelming, and i slept couple of time, so that is also like bit less than 1/2 done.

--- web dev harkirat/week-13.2/src/App.jsx
+++ web dev harkirat/week-13.2/src/App.jsx
@@ -1,16 +1,20 @@
-import './App.css'
-import SidebarClass1 from './components/answer/1-basic-project'
+import './App.css';
+import SidebarClass1 from './components/answer/1-basic-project';
+import { Sidebar } from './components/Sidebar';
+import { Sidebar2Transition } from './components/sidebars/Sidebar2Transition';
+import { Sidebar4 } from './components/sidebars/Sidebar4';
 
 function App() {
-
+  // console.log("first")
   return (
     <div className='h-screen bg-blue-700'>
-      <SidebarClass1 />
+      {/* <SidebarClass1 /> */}
 
-      {/* <Sidebar1 /> */}
+      {/* <Sidebar2Transition /> */}
       {/* <Sidebar4 /> */}
+      <Sidebar />
     </div>
-  )
+  );
 }
 
 export default App
