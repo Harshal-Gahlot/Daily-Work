09f377c864328d8fab8ad70f1ed5c0a11a5094dc
Harshal <gahlotharshal@gmail.com>
2025-11-19 16:55:21 +0530

    completed cohort web2 week14.1 i.e. first typescript lesson

---
+++ web dev harkirat/web2 week 14 starting learning typescript/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function ph(name) {
+    console.log("hello,", name);
+}
+ph("harshal");
+//# sourceMappingURL=index.js.map
